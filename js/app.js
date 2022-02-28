24b1a38b8d95ed047c3b8a2fa0cc074a4515bf22
JunayetTamim <junayettamim186@gmail.com>
2022-02-28 22:08:17 +0600

    data load

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('connect');
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear data
+    searchField.value = '';
+    if (searchText == '') {
+        // please write something to display
+    }
+    else {
+        // load data
+        const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => console.log(data.data))
+    }
+}
